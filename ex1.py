91c63c08d8220588f610cfbd751102dd964c4938
ROHIT KUMAR SINGH <rohitsingh62076@gmail.com>
2026-06-04 16:24:41 +0530

    Add files via upload

---
+++ ex1.py
@@ -0,0 +1,40 @@
+# Q1
+print("Q1 =", 10 + 5 * 2 - 3 ** 2)
+
+# Q2
+print("Q2 Floor Division =", 100 // 7)
+print("Q2 Modulus =", 100 % 7)
+
+# Q3
+print("Q3 =", 2 ** 10)
+
+# Q4
+x = 50
+x += 25
+x -= 10
+x //= 5
+print("Q4 =", x)
+
+# Q5
+print("Q5 =", (15 > 10) and (8 != 8))
+
+# Q6
+print("Q6 =", not (5 == 5 or 3 < 2))
+
+# Q7
+result = (18 / 4) + (18 // 4) + (18 % 4)
+print("Q7 =", result)
+
+# Q8
+y = 3
+y *= 4
+y **= 2
+print("Q8 =", y)
+
+# Q9
+print("Q9 =", (7 > 3) or (10 < 5) and not (2 == 2))
+
+# Q10
+a = 6
+b = 4
+print("Q10 =", (a + b) ** 2 - (a ** 2 + b ** 2))
