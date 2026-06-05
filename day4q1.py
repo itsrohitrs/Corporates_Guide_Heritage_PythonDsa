592303b71666816c612625fcce4470d7dffac908
ROHIT KUMAR SINGH <rohitsingh62076@gmail.com>
2026-06-05 16:20:25 +0530

    Add files via upload

---
+++ day4q1.py
@@ -0,0 +1,14 @@
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+division = num1 / num2
+
+print("\nResults:")
+print("Addition =", addition)
+print("Subtraction =", subtraction)
+print("Multiplication =", multiplication)
+print("Division =", division)
